ac0bd55f081e80634833f2d601942de93c18c052
Tal Yaniv <tal@reali.com>
2018-10-27 19:03:21 +0300

    initial commit

---
+++ README.md
@@ -0,0 +1,72 @@
+# Lightweight shell script for daily kubernetes jobs
+
+Usage:
+
+```
+./kubex.sh <arg1> <optional arg2>
+```
+
+You may need to change the permissions for the script to run
+```
+chmod 700 kubex.sh
+```
+
+* reduces the hassle of getting a login token, copying to clipboard and opening a proxy session
+* easily switching between contexts without needing to remember complex context name (e.g. in AWS EKS)
+  -> return types must match! (deduced from error case)
+
+Copyright (c) 2018 Tal Yaniv
+
+License: http://opensource.org/licenses/MIT
+
+
+## arguments
+```
+l or --list lists-contexts   list all kubernetes contexts
+                             use the index numbers to switch conexts with --context command
+
+t or --token                 generates a login token and copies to clipboard
+
+c <context index>
+or --context <context index> switches to a context from the list
+                             use l or --list to see the list
+
+p or --proxy                 gets a token and copies to clipboard
+                             open Mac Chrome app with the login page
+                             starts a proxy session
+```
+
+## note about the --proxy method
+
+This opens Mac Chrome browser. If you prefer another browser or use Linux, just change the app path from ```/Applications/Google Chrome.app``` to your favorite app.
+
+## example
+
+list contexts and switch to production:
+```
+$ ./kubex.sh --list-contexts
+1 - arn:aws:eks:us-east-1:439989238490:cluster/staging
+2 - arn:aws:eks:us-east-1:439989238490:cluster/production
+
+$ ./kubex.sh --context 2
+Switched to context "arn:aws:eks:us-east-1:439989238490:cluster/production".
+```
+
+## tip - create a symbolic link in /usr/local/bin/
+
+Instead of going to the directory or copying this script all around - simply create a link for it in /usr/local/bin/
+
+For example if the script is downloaded to `~/Downloads/kubex/kubex.sh`
+
+```
+ln -s ~/Downloads/kubex/kubex.sh /usr/local/bin/kubex
+```
+
+Then, from anywhere you can always
+```
+$ kubex l
+1 - arn:aws:eks:us-east-1:439989238490:cluster/staging
+2 - arn:aws:eks:us-east-1:439989238490:cluster/production
+```
+
+Easier!
---
+++ kubex.sh
@@ -0,0 +1,150 @@
+#!/bin/bash
+
+# Copyright (c) 2018 Tal Yaniv
+
+
+########################
+#   internal methods   #
+########################
+
+# get_kube_token will get your kubernetes (may take a few seconds) and copy it to your clipboard
+
+function get_kube_token {
+    echo "getting token..."
+    RES=`kubectl -n kube-system describe secret $(kubectl -n kube-system get secret | grep eks-admin | awk '{print $1}')`
+    echo ${RES#*token: } | pbcopy
+    echo "token copied to clipboard"
+}
+
+
+# get_contexts will scan your ~/.kube/config file, prepare a list of contexts and will store them in contexts variable
+
+function get_contexts {
+
+    contexts=() # stores an array of contexts
+    found_contexts=false # flags finding the "contexts" label
+    contexts_count=0
+
+    # iterating over the config file
+    while IFS='' read -r line || [[ -n "$line" ]]; do
+        if [ "$found_contexts" = true ]; then
+            if [[ $line == *"  name: "* ]]; then
+                line_content=${line#*"  name: "}
+                contexts+=("$line_content")
+                contexts_count=$(( $contexts_count + 1 ))
+            fi
+        fi
+        if [ "$line" = users: ]; then
+            break
+        fi
+        if [ "$line" = contexts: ]; then
+            found_contexts=true
+        fi
+    done < ~/.kube/config
+
+}
+
+
+
+##########################
+#   commands execution   #
+##########################
+
+
+
+# ./kubex.sh t
+# or
+# ./kubex.sh --token
+
+# gets a token and stores in clipboard
+
+if [ "$1" == "t" ] || [ "$1" == "--token" ]; then
+    get_kube_token
+fi
+
+
+# ./kubex.sh p
+# or
+# ./kubex.sh --proxy
+
+# gets a token
+# stores in clipboard
+# opens a Chrome (Mac) browser page and navigates to the login screen
+# starts a proxt session
+
+if [ "$1" == "p" ] || [ "$1" == "--proxy" ]; then
+    get_kube_token
+    /usr/bin/open -a "/Applications/Google Chrome.app" "http://localhost:8001/api/v1/namespaces/kube-system/services/https:kubernetes-dashboard:/proxy/#!/login"
+    kubectl proxy
+fi
+
+
+# ./kubex.sh c <conext index>
+# or
+# ./kubex.sh --context <context index>
+
+# switches context
+# to list all contexts use:
+# ./kubex.sh --list-contexts 
+
+# example - to list contexts and switch to production:
+# $ ./kubex.sh --list-contexts
+# 1 - arn:aws:eks:us-east-1:439989238490:cluster/staging
+# 2 - arn:aws:eks:us-east-1:439989238490:cluster/production
+# $ ./kubex.sh --context 2
+
+if [[ "$2" != "" && ( "$1" == "c" || "$1" == "--context" ) ]]; then
+    get_contexts
+    CONTEXT=${contexts[$(( $2 - 1 ))]}
+    if [ "$CONTEXT" == "" ]; then
+        echo "context out of scope (1~$contexts_count)"
+    else
+        kubectl config use-context "$CONTEXT"
+    fi
+fi
+
+
+
+# ./kubex.sh l
+# or
+# ./kubex.sh --list-contexts
+
+# lists all context in ~/.kube/config
+
+# example
+# $ ./kubex.sh --list-contexts
+# 1 - arn:aws:eks:us-east-1:439989238490:cluster/staging
+# 2 - arn:aws:eks:us-east-1:439989238490:cluster/production
+
+if [ "$1" == "l" ] || [ "$1" == "--list-contexts" ]; then
+    get_contexts
+
+    context_index=0
+    while [ "$context_index" -lt "$contexts_count" ]
+    do
+        echo $(( $context_index + 1 )) - ${contexts[$context_index]}
+        context_index=$(( $context_index + 1 ))
+    done
+fi
+
+if [ $# -eq 0 ]; then
+    echo "kubex is a lightweight shell script for daily kubernetes jobs"
+    echo
+    echo "usage:"
+    echo "./kubex.sh <arg1> <optional arg2>"
+    echo "example:"
+    echo ".kubex.sh l"
+    echo
+    echo "l or --list lists-contexts   list all kubernetes contexts"
+    echo "                             use the index numbers to switch conexts with --context command"
+    echo
+    echo "t or --token                 generates a login token and copies to clipboard"
+    echo
+    echo "c <context index>"
+    echo "or --context <context index> switches to a context from the list"
+    echo "                             use l or --list to see the list"
+    echo
+    echo "p or --proxy                 gets a token and copies to clipboard"
+    echo "                             open Mac Chrome app with the login page"
+    echo "                             starts a proxy session"
+fi
